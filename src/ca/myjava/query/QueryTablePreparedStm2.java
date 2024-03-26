babcdc2a771084ecd70d43700866140e17269e95
T6n9y <88888117+T6n9y@users.noreply.github.com>
2024-03-26 14:51:50 -0400

    Update QueryTablePreparedStm2.java

--- src/ca/myjava/query/QueryTablePreparedStm2.java
+++ src/ca/myjava/query/QueryTablePreparedStm2.java
@@ -5,88 +5,73 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
-
-import oracle.info.OracleInfo;
 
 
 public class QueryTablePreparedStm2 {
+	
 	public static void main(String[] args) {
-		Connection connection = null;
-		PreparedStatement pstmt = null;
+			
+			String sql = "SELECT Country, LifeExpectancy FROM countrydata where LifeExpectancy BETWEEN ? AND ?";
 
 		try {
 			
-			// load driver
-			Class.forName(OracleInfo.DRIVER_CLASS_MYSQL);
-			System.out.println("driver is loaded...\n");
-			
-			// connection to database
-			connection = DriverManager.getConnection(OracleInfo.URL, OracleInfo.USER, OracleInfo.PASSWORD);
-			System.out.println("database is connected...\n");
-			
-			String sql = "SELECT * FROM country WHERE lifeexpectancy BETWEEN ? AND ?";
-            pstmt = connection.prepareStatement(sql);
-
-			System.out.println("Prepared statement created...");
-			
-			// read input of where conditions
-			Scanner scan = new Scanner(System.in);
-			
-			System.out.println("Input min age: ");
-			int minAge = scan.nextInt();
-
-			System.out.println("Input max age: ");
-			int maxAge = scan.nextInt();
-
-			pstmt.setInt(1, minAge);
-			pstmt.setInt(2, maxAge);
-			
-			System.out.println("data accepted ...\n");
-
-			// execute query
-			ResultSet resultSet = pstmt.executeQuery();
-			
-			// print output
-			System.out.printf("Country\t\tLife Expectancy\n");
-			while(resultSet.next()) {
-				System.out.println(resultSet.getString(1) + "\t\t" + resultSet.getString(2));
-			}
-			
-			scan.close();
-		}
-		catch (InputMismatchException ime) {
-			ime.getStackTrace();
-		}
-		catch (SQLException se) {
-			se.getStackTrace();
-		}
-		catch (ClassNotFoundException cnfe) {
-			cnfe.getStackTrace();
-		}
-		catch (Exception e) {
-			e.getStackTrace();
-		}
-		finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-			}
-			catch(SQLException se) {
-				se.printStackTrace();
-			}
-			
-			try {
-				if(connection != null) {
-					connection.close();
-				}
-			}
-			catch(SQLException se) {
-				se.printStackTrace();
-			}
+			//LOADING THE DRIVER
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("Drivers loaded successsfully");
+		}catch(ClassNotFoundException e) {
+			System.out.println(e.getMessage());
 		}
 		
+		try {
+			//ESTABLISHING A CONNECTION
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@calvin.humber.ca:1521:grok", "N01579272", "oracle");
+			System.out.println("Connection is Successful");
+			
+			//USING PREPARED STATEMENT
+			
+			PreparedStatement ps = conn.prepareStatement(sql);
+			
+			//GETTING INPUT FROM THE USERS
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter the min age: ");
+			int minAge = scan.nextInt();
+			
+			System.out.println("Enter the max age: ");
+			int maxAge = scan.nextInt();
+			
+			ps.setInt(1, minAge);
+			ps.setInt(2, maxAge);
+			
+			//EXCECUTING THE QUERY
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				String countryName = rs.getString("Country");
+				float lifeExpectancy = rs.getFloat("LifeExpectancy");	
+				System.out.println("Country : " + countryName);
+				System.out.println("Life Expectancy" + lifeExpectancy);
+			}
+			rs.close();
+			ps.close();
+			scan.close();
+			
+			
+			//CLOSING THE CONNECTION
+			conn.close();
+			System.out.println();
+			System.out.println("Connection Closed successfully");
+			
+			
+		}catch(SQLException e) {
+		    System.out.println("SQLException occurred: " + e.getMessage());
+		    e.printStackTrace(); 
+		}
+
+		
+		
+		
 	}
+	
+	
+
 }
